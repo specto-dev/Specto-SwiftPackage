27c21edb0d22b658b774425d061b02940548e418
specto-ci <ci@specto.dev>
2020-11-03 21:44:54 +0000

    Release Specto for iOS 0.1.0-RC14

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Specto",
-            url: "https://users.specto.dev/binary-releases/xcframework/0.1.0-RC13.zip",
-            checksum: "a558373162671d60571d18564f4d06fa69f21db5a6d9fef080214d5c761b7089"),
+            url: "https://users.specto.dev/binary-releases/xcframework/0.1.0-RC14.zip",
+            checksum: "83d5bd74ccf965337f583f1c5e098983612f309147315aae13c7c03ef0764e0f"),
     ]
 )
