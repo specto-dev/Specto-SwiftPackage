ed1b843b1aefd9183b00809cf1893be83b82e021
Andrew McKnight <amcknight2718@gmail.com>
2020-12-15 11:37:31 -0900

    Release Specto for iOS 0.1.0+c5c4f06cb

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Specto",
-            url: "https://users.specto.dev/binary-releases/xcframework/0.1.0+69419b20.zip",
-            checksum: "776d97ecc1f8613b557d6d48f6ac71ca3414ff69e5f8f1ba48f7a40be1f44e74"),
+            url: "https://users.specto.dev/binary-releases/xcframework/0.1.0+c5c4f06cb.zip",
+            checksum: "258f19406304be6e8ac4f0a666f4c1301fc7c705ee35ca6df289497a66056742"),
     ]
 )
