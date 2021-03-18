950f4becabbfaa16c5e494813ec4f64f1d0a1a8e
Andrew McKnight <andrew@specto.dev>
2021-03-18 14:32:37 -0800

    update to new release URL (#3)

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Specto",
-            url: "https://users.specto.dev/binary-releases/ios/sdk/release/xcframework/0.2.0.zip",
+            url: "https://releases.specto.dev/ios/sdk/release/xcframework/0.2.0.zip",
             checksum: "20f4cd5320adbf81f04d9ba374d9514da805a93e74f8e22191eaa4415aa473b0"),
     ]
 )
