0b5b3b9532a887887fb659802f0a13418c1825db
Indragie Karunaratne <indragie@specto.dev>
2021-04-08 00:15:17 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,9 +11,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Specto",
-            
-            url: "https://releases..specto.dev/ios/sdk/alpha/xcframework/0.2.3+050754094.zip",
-            
+            url: "https://releases.specto.dev/ios/sdk/alpha/xcframework/0.2.3+050754094.zip",
             checksum: "ea47a2482afb10005d4b4a03c016bb7106d6798b49e7f50d2c086a86e2d6de61"),
     ]
 )
