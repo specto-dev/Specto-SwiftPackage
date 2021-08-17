919436b3e963a46d69567f8808c6bb7badc233d0
specto-ci <ci@specto.dev>
2021-08-17 00:41:34 +0000

    Release Specto for iOS 1.3.2+8c9e60897

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
         .binaryTarget(
             name: "Specto",
             
-            url: "https://releases.specto.dev/ios/sdk/alpha/xcframework/1.3.2+5869bbb1f.zip",
+            url: "https://releases.specto.dev/ios/sdk/alpha/xcframework/1.3.2+8c9e60897.zip",
             
-            checksum: "e0c92df6f55146acb5f0bc09ad8c5b63677be190495f1f0ec4e5a8f45621752d"),
+            checksum: "a87bf7bd7e9418385583cbb32891165897ba31216cb5ccec97ced13fc1b0ec4e"),
     ]
 )
